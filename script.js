a301baa33e1a8a904783a771d803199112428514
Ayan <ayangaming0454@gmail.com>
2024-12-01 02:21:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+    var menu = document.getElementById("navbar-items");
+    menu.classList.toggle("show"); // Toggle the "show" class
+}
+// Start the typewriter effect
+type();
+
+AOS.init();
